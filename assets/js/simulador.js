8b6ad33279b5af3193fd0c2bc5335b9faa69f99b
MsMatias <matias.miranda.ms@gmail.com>
2018-05-23 14:13:48 -0300

    Cambios bolitas

--- assets/js/simulador.js
+++ assets/js/simulador.js
@@ -52,7 +52,84 @@ function CalcularCapacitor () {
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var img = new Image();
-img.src = "assets/images/fondo_canvas.png";
-img.onload = function(){
-  ctx.drawImage(img, 0, 0, 700, 400);
+//img.src = "assets/images/fondo_canvas.png";
+//img.onload = function(){
+// ctx.drawImage(img, 0, 0, 700, 400);
+//}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
+
+//CLASE barra
+var barra = {
+	ancho: 80,
+	alto: 15,
+	x: (canvas.width)/2,
+	y: canvas.height-30,
+	color: '#FFF000',
+	dibujar: function (ancho) {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x - ancho, this.y, this.ancho, this.alto);
+	}
+
+};
+
+//CLASE bolita
+var bolita = {
+	radio: 20,
+	x: (canvas.width)/2,
+	y: canvas.height-60,
+	color: '#000FFF',
+	dibujar: function () {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.arc(this.x, this.y, this.radio, this.radio, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.closePath();
+	}
+}
+
+//CLASE bloques
+bloques = {
+	contenido: [],
+	columnas: 5,
+	filas: 3,
+	ancho: 90,
+	alto: 25,
+	margentop: 50,
+	margenleft: 50,
+	init: function(){
+		for(var f = 0; f < this.filas; f++){
+			for(var c = 0; c < this.columnas; c++){
+				this.contenido.push(
+					{
+						color: getRandomColor(),
+						x: this.margentop + (c*this.ancho) + (c*5),
+						y: this.margenleft + (f*this.alto) + (f*5),
+						existe: true
+					});
+			}
+		}
+	},
+	dibujar: function () {
+		for(bloque of this.contenido){
+				ctx.fillStyle = bloque.color;
+				ctx.fillRect(bloque.x, bloque.y, this.ancho, this.alto);		
+		}
+	}
+}	
+
+barra.dibujar(barra.ancho/2);
+bolita.dibujar();
+bloques.init();
+bloques.dibujar();
+/*for(var i = 0; i < 35; i++){
+	ctx.fillStyle="#FF0000";
+	ctx.fillRect(i * 35, 3,32,15);
+}*/
